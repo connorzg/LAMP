b2d109cd9a25a51e085fe071f46ac848042f6629
Connor Garber <czaneg@gmail.com>
2016-09-18 18:08:34 -0500

    submitted prework

--- memgame/main.js
+++ memgame/main.js
@@ -1,11 +1,45 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "queen", "queen", "king", "king", "king", "king"];
+var cardsInPlay = []
+var points = document.getElementById('points')
+var score = points.innerHTML;
 
-if (cardTwo === cardFour) {
-	alert('You found a match!')
+function reset(){
+	board.innerHTML = ('')
+	createBoard();
 }
-else {
-	alert('Sorry, try again.')
+var board = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var newCard = document.createElement('div');
+	newCard.className = 'card';
+	newCard.setAttribute('data-card', cards[i]);
+	newCard.addEventListener('click', isTwoCards);
+	board.appendChild(newCard);
+	}
 }
+var isTwoCards = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png'>";
+	} else {
+		this.innerHTML = "<img src='queen.png'>";
+	}
+	if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+	cardsInPlay = [];
+	}
+}
+
+var isMatch = function(cards){
+	if (cards[0] === cards[1]) {
+		alert('You found a match!');
+		score++;
+		points.innerHTML = score;
+	}
+	else {
+		alert('Sorry, try again.')
+	}
+}
+createBoard();
